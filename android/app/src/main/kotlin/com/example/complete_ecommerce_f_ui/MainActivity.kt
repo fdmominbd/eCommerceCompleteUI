553166c583c6785e465d14b39a054e492ada001d
Emam999 <mdemamshikder1@gmail.com>
2022-01-16 05:07:13 -0600

    commit 16.1.22

---
+++ android/app/src/main/kotlin/com/example/complete_ecommerce_f_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.complete_ecommerce_f_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:complete_ecommerce_f_ui/screens/complete_profile/components/body.dart';
+import 'package:flutter/material.dart';
+
+
+import '../../size_config.dart';
+
+class SplashScreen extends StatelessWidget {
+  static String routeName = "/splash";
+  @override
+  Widget build(BuildContext context) {
+    // You have to call it on your starting screen
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
